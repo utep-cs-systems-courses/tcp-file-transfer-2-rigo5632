2f0116a4697f2a47fd719f6a068e08f34795dfc4
rigo5632 <rmquiroz5@gmail.com>
2020-10-16 14:40:49 -0600

    added test files and comments

---
+++ file-transfer-lab/Fork_Base/client/Client.py
@@ -0,0 +1,99 @@
+#! /usr/bin/python3
+import os, socket, sys, re, time
+sys.path.append('../../../lib')
+import params
+sys.path.append('../../../framed-echo')
+from framedSock import framedReceive, framedSend
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', '127.0.0.1:50001'),
+    (('-p', '--proxy'), 'proxy', False),
+    (('-?', '--usage'), 'usage', False)
+)
+
+paramMap = params.parseParams(switchesVarDefaults)
+server, proxy, usage = paramMap['server'], paramMap['proxy'], paramMap['usage']
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(':', server)
+    serverPort = int(serverPort)
+except:
+    print('ERROR: Can\'t parse server:port from %s' %server)
+    sys.exit(1)
+
+
+def fileExists(fileName):
+    try:
+        fileSource = './files/' + fileName
+        file = open(fileSource, 'rb')
+    except FileNotFoundError:
+        print('Error: Could not find file')
+        return None, False
+    except PermissionError:
+        print('Error: Invalid Address')
+        return None, False
+    return file, file.readlines()
+
+def createPayload(key, filename, data):
+    for i in range(len(data)):
+        description = (key + ':' + filename + ':').encode()
+        data[i] = description + data[i]
+    return data
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverHost, serverPort))
+while True:
+    try:
+        key = framedReceive(clientSocket, False).decode()
+        print('Server File Key: %s' %key)
+    except ConnectionResetError:
+        print('Error: Lost Connection to Server')
+        break
+    
+    userInput = str(input('> '))
+    tokens = re.split(' +', userInput)
+
+    if len(tokens) == 3:
+        command = tokens[0]
+        localFileName = tokens[1]
+        remoteFileName = tokens[2]
+    elif len(tokens) == 2:
+        command = tokens[0]
+        localFileName = tokens[1]
+        remoteFileName = localFileName
+    elif len(tokens) == 1 and tokens[0] == 'exit':
+        framedSend(clientSocket, tokens[0].encode(), False)
+        break
+    else:
+        command = None
+        localFileName = None
+        remoteFileName = None
+    
+    if len(tokens) >= 2 and command == 'put':
+        file, data = fileExists(localFileName)
+
+        data = createPayload(key, remoteFileName, data) if file else False
+
+        if file and data:
+            for payload in data:
+                print('Sending: %s' %payload, end = ' ')
+                framedSend(clientSocket, payload, False)
+            file.close()
+            key = str(int(key) + 1)
+            framedSend(clientSocket, key.encode(), False)
+        elif file and not data:
+            print('%s has no data' %localFileName)
+            framedSend(clientSocket, key.encode(), False)
+        else:
+            framedSend(clientSocket, key.encode(), False)
+    else:
+        print('Invalid Syntax!')
+        print('put <local-file-name> <remote-file-name>')
+        framedSend(clientSocket, key.encode(), False)
+clientSocket.close()
+
+
+
---
+++ file-transfer-lab/Fork_Base/server/Server.py
@@ -0,0 +1,83 @@
+#! /usr/bin/python3
+
+import os, socket, re, sys
+sys.path.append('../lib')
+import params
+from framedSock import framedReceive, framedSend
+
+# params
+switchesVarDefaults = (
+    (('-l', '--listentPort'), 'listenPort', 50001),
+    (('-p', '--proxy'), 'proxy', False),
+    (('-?', '--usage'), 'usage', False)
+)
+
+paramMap = params.parseParams(switchesVarDefaults)
+listenPort, proxy, usage = paramMap['listenPort'], paramMap['proxy'], paramMap['usage']
+
+
+# usage of params
+if usage:
+    params.usage()
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(('', listenPort))
+serverSocket.listen(5)
+
+key = '-1'
+while True:
+    key = str(int(key) + 1)
+    connection, address = serverSocket.accept()
+    framedSend(connection, key.encode(), False)
+    filename = ''
+    content = ''
+
+    if not os.fork():
+        files = os.listdir('./Files')
+        while True:
+            data = framedReceive(connection, False)
+            if not data: # file did not finsih writing to server
+                if filename not in files and filename != None:
+                    fileSource = './Files/' + filename
+                    os.remove(fileSource)
+                    print('Deleting File: %s' %filename)
+                break
+            data = data.decode()
+
+            # unpack data from client
+            payload = re.split(':', data)
+            serverFiles = os.listdir('./Files')
+            # update files in server
+            if len(payload) == 1:
+                key = payload[0]
+                files = os.listdir('./Files')
+                filename = None
+                content = None
+                framedSend(connection, key.encode(), False)
+                print('Sending File Key...')
+                print(files)
+            else:
+                key = payload[0]
+                filename = payload[1]
+                content = payload[2]
+
+            #print(files)
+            # write file
+            if filename not in files and filename is not None:
+                fileDestination = './files/' + filename
+                file = open(fileDestination, 'a')
+
+                #print('Writing to %s' %filename)
+
+                file.write(content)
+                file.close()
+                framedSend(connection, b'Sucess', False)
+            else: # file was fond in server
+                if filename:
+                    print('%s found in server' %filename)
+                    print('Not writing data...')
+                    framedSend(connection, b'Error', False)
+                    break
+        sys.exit(0)
+        connection.close()
+
